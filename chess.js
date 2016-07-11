3f94f409d97151e63c8f875fd90fef38d4cd2cf7
Marty Schumacher <schumarty@gmail.com>
2016-07-10 21:31:58 -0500

    define event handler so that pieces can be moved by clicking!

--- chess.js
+++ chess.js
@@ -13,8 +13,35 @@ posToCss = function(position) {
 	return('.rank_' + position.split("")[1] + ' > .file_' + position.split("")[0]);
 }
 
-cssToPos = function(cssSel) {
-	var result = cssSel.match(/\.rank_(\d) > \.file_([a-hA-H])/);
+cssToPos = function(file, rank) {
+	var result = file.match(/file_([a-hA-H])/)[1]
+		+rank.match(/rank_(\d)/)[1];
 
-	return(result[2] + result[1]);
+	return(result);
 }
+
+// placeholder for piece if one is selected
+var piece = {
+	html: "",
+	boardPosition: ""
+};
+$("td").click(function () {
+	// only the selected spot will be orange
+	$("td").each(function() {
+		this.removeAttribute("style");
+	});
+	$(this).css("background-color", "orange");
+
+	if (piece.html) {
+		var newPosition = cssToPos(this.className, this.parentElement.className);
+		// show that piece has been de-selected
+		if (newPosition === piece.boardPosition) {
+			this.removeAttribute("style");
+		}
+		movePiece(piece.boardPosition, newPosition);
+		piece.html = "";
+	} else {
+		piece.html = $(this).html();
+		piece.boardPosition = cssToPos(this.className, this.parentElement.className);
+	}
+});
