1cce47c127b09b93d5893b7157d5e0396fdda972
Marty Schumacher <schumarty@gmail.com>
2016-08-19 19:16:20 -0500

    Turn chess_rules.js into a very messy object

--- chess_rules.js
+++ chess_rules.js
@@ -1,73 +1,99 @@
+Chess = function() {
 // We're going to store everything in variables that reseble FEN notation
-var chessboard = [
-	"rnbqkbnr",
-	"pppppppp",
-	"        ",
-	"        ",
-	"        ",
-	"        ",
-	"PPPPPPPP",
-	"RNBQKBRR"
-];
-
-var activeColor = "w";
-
-var canCastle = {
-	"K": true,
-	"Q": true,
-	"k": true,
-	"q": true
-};
-
-var enPassant = "";
-
-var halfMoveClock = 0;
-var fullMoveClock = 1;
-
-isOnBoard = function(rank, file) {
-	// TODO: eventually include support for algebraic notation
-  	if (rank >= 0 && rank <= 7 && file >= 0 && file <= 7) {
-		return(true);
-	} else {
-		return(false);
-	}
-}
-
-getColor = function(piece) {
-	if (piece === " ") {
-		return(undefined);
-	}
-	if (piece === piece.toUpperCase()){
-		return("w");
-	} else {
-		return("b");
-	}
-}
-
-fenToAn = function(rank, file) {
-	var algebraic = String.fromCharCode(97 + file);
-	algebraic += (8 - rank);
-	return(algebraic);
-}
-
-getValidMoves = function(rank, file) {
-	var possibleMoves = [
-		[rank - 1, file - 1],
-		[rank - 1, file],
-		[rank - 1, file + 1],
-		[rank, file - 1],
-		[rank, file + 1],
-		[rank + 1, file - 1],
-		[rank + 1, file],
-		[rank + 1, file + 1]
+	this.board = [
+		"rnbqkbnr",
+		"pppppppp",
+		"        ",
+		"        ",
+		"        ",
+		"        ",
+		"PPPPPPPP",
+		"RNBQKBRR"
 	];
-	var validMoves = [];
-	for (i = 0; i < possibleMoves.length; i++) {
-		var pos = {r: possibleMoves[i][0], f: possibleMoves[i][1]};
-		if (isOnBoard(pos.r, pos.f)
-			&& activeColor !== getColor(chessboard[pos.r][pos.f])) {
-			validMoves.push(fenToAn(pos.r, pos.f));
+
+	this.activeColor = "w";
+
+	this.canCastle = {
+		"K": true,
+		"Q": true,
+		"k": true,
+		"q": true
+	};
+
+	this.enPassant = "";
+
+	this.halfMoveClock = 0;
+	this.fullMoveClock = 1;
+
+// These are the functions used to interact with the object
+// All board positions in and out of the functions use algebraic notation
+
+	this.getMoves = function(alg) {
+		var pos = this._anToFen(alg);
+		var fenMoves = this._getValidMoves(pos.r, pos.f);
+		var anMoves = [];
+		for (i = 0; i < fenMoves.length; i++) {
+			anMoves.push(this._fenToAn(fenMoves[i]));
+		}
+		return(anMoves);
+	}
+
+// These are helper functions for the internal dirty work
+// All inputs and outputs describing board position use object of coordinates
+
+	this._isOnBoard = function(rank, file) {
+		if (rank >= 0 && rank < this.board.length
+			&& file >= 0 && file < this.board[0].length)
+		{
+			return(true);
+		} else {
+			return(false);
 		}
 	}
-	return validMoves;
-}
+
+	this._getColor = function(piece) {
+		if (piece === " ") {
+			return(undefined);
+		}
+		if (piece === piece.toUpperCase()){
+			return("w");
+		} else {
+			return("b");
+		}
+	}
+
+	this._fenToAn = function(posObj) {
+		var algebraic = String.fromCharCode(97 + posObj.f);
+		algebraic += 8 - parseInt(posObj.r);
+		return(algebraic);
+	}
+	
+	this._anToFen = function(algebraic) {
+		var file = (algebraic[0].charCodeAt(0) - 97);
+		var rank = 8 - algebraic[1];
+		return({r: rank, f: file});
+	}
+
+	this._getValidMoves = function(rank, file) {
+		var possibleMoves = [
+			[rank - 1, file - 1],
+			[rank - 1, file],
+			[rank - 1, file + 1],
+			[rank, file - 1],
+			[rank, file + 1],
+			[rank + 1, file - 1],
+			[rank + 1, file],
+			[rank + 1, file + 1]
+		];
+		var validMoves = [];
+		for (i = 0; i < possibleMoves.length; i++) {
+			var pos = {r: possibleMoves[i][0], f: possibleMoves[i][1]};
+			if (this._isOnBoard(pos.r, pos.f)
+				&& this.activeColor !== this._getColor(this.board[pos.r][pos.f]))
+			{
+				validMoves.push(pos);
+			}
+		}
+		return validMoves;
+	}
+};
